e1e69cf2c5ad29d53a3c8c275d5ceea2007d31b0
Yishuai Li <yishuai@cis.upenn.edu>
2017-04-04 14:18:46 -0400

    extend to 3D and add field operations

--- src/Physics.hs
+++ src/Physics.hs
@@ -1,6 +1,6 @@
-module Physics (force) where
+module Physics (gravity, (./)) where
 
-import Types
+import           Types
 
 -- For floating point comparisons
 epsilon :: Float
@@ -10,18 +10,44 @@ epsilon = 0.001
 bigG :: Float
 bigG = 6.67428e-11        -- in m^3 kg^(-1) s^(-2)
 
+newtype Field = Field (Particle -> Force)
+
+gravity :: Particle -> Field
+gravity (Particle (Mass m0) (Pos x0 y0 z0) _) = Field f
+  where
+    f (Particle (Mass m1) (Pos x1 y1 z1) _)
+      | d < epsilon = Force 0 0 0
+      | otherwise = Force (absAccel * dx / d) (absAccel * dy / d) (absAccel * dz / d)
+      where
+        dx       = x1 - x0
+        dy       = y1 - y0
+        dz       = z1 - z0
+        dsqr     = dx * dx + dy * dy + dz * dz
+        d        = sqrt dsqr
+        absAccel = bigG * m0 * m1 / dsqr
+
+(./) :: Force -> Mass -> Accel
+(Force x y z) ./ (Mass m) = Acc (x / m) (y / m) (z / m)
+
+(.+) :: Force -> Force -> Force
+Force x1 y1 z1 .+ Force x2 y2 z2 = Force (x1 + x2) (y1 + y2) (z1 + z2)
+
+(.+.) :: Field -> Field -> Field
+Field f1 .+. Field f2 = Field (\x -> f1 x .+ f2 x)
+
 -- Given two particles, determine the acceleration exerted by the second on the first.
 --
--- As a special case, the force is zero if both particles are closer than 
+-- As a special case, the force is zero if both particles are closer than
 -- a minimal epsilon distance.
 --
 force :: Particle -> Particle -> Accel
-force (Particle (Mass _) (Pos x1 y1) _) (Particle (Mass m2) (Pos x2 y2) _)
-  | d < epsilon = Acc 0 0
-  | otherwise   = Acc (absAccel * dx / d) (absAccel * dy / d) 
+force (Particle (Mass _) (Pos x1 y1 z1) _) (Particle (Mass m2) (Pos x2 y2 z2) _)
+  | d < epsilon = Acc 0 0 0
+  | otherwise   = Acc (absAccel * dx / d) (absAccel * dy / d) (absAccel * dz / d)
   where
     dx       = x2 - x1
     dy       = y2 - y1
-    dsqr     = dx * dx + dy * dy
+    dz       = z2 - z1
+    dsqr     = dx * dx + dy * dy + dz * dz
     d        = sqrt dsqr
     absAccel = bigG * m2 / dsqr
--- src/Types.hs
+++ src/Types.hs
@@ -1,37 +1,38 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Types (
-  Mass(..), Position(..), Velocity(..), Accel(..), Particle(..), World(..),
+  Mass(..), Position(..), Velocity(..), Accel(..), Particle(..), World(..), Force(..)
 ) where
 
-import Data.Aeson
-import GHC.Generics
+import           Data.Aeson
+import           GHC.Generics
 
 newtype Mass   = Mass Float deriving (Show, Read, Generic) -- in kilograms
-data Position  = Pos { posx :: Float, posy :: Float } deriving (Show, Read, Generic) -- in meters
-data Velocity  = Vel { velx :: Float, vely :: Float } deriving (Show, Read, Generic) -- in meters/second
-data Accel     = Acc { accx :: Float, accy :: Float } deriving (Show, Read, Generic) -- in meters/second^2
+data Position  = Pos { posx :: Float, posy :: Float, posz :: Float } deriving (Show, Read, Generic) -- in meters
+data Velocity  = Vel { velx :: Float, vely :: Float, velz :: Float } deriving (Show, Read, Generic) -- in meters/second
+data Accel     = Acc { accx :: Float, accy :: Float, accz :: Float } deriving (Show, Read, Generic) -- in meters/second^2
+data Force     = Force { fx :: Float, fy :: Float, fz :: Float} deriving (Show, Read, Generic) -- in newton
 
 data Particle = Particle {
     pmass :: Mass
   , ppos  :: Position
   , pvel  :: Velocity
 } deriving (Show, Read, Generic)
-  
--- The world state consists of three scaling factors and a set of particles.  
+
+-- The world state consists of three scaling factors and a set of particles.
 --
 -- * The first scaling factor determines which fraction of a pixel represents one meter.
--- * The second scaling factor determines which fraction of a pixel represents 
+-- * The second scaling factor determines which fraction of a pixel represents
 --   one kilogram when determining the radius of the circle representing a particle.
--- * The third scaling factor determines how many simulated seconds correspond to 
+-- * The third scaling factor determines how many simulated seconds correspond to
 --   one second of real time.
 --
 data World = World {
-    seqNum  :: Int   -- sequence number to serialize communications
-  , pixInM  :: Float -- fraction of a pixel corresponding to world meter
-  , pixInKg :: Float -- fraction of a pixel corresponding to world kg
+    seqNum        :: Int   -- sequence number to serialize communications
+  , pixInM        :: Float -- fraction of a pixel corresponding to world meter
+  , pixInKg       :: Float -- fraction of a pixel corresponding to world kg
   , usrToWrldTime :: Float -- user time in s to world time
-  , parts   :: [Particle]
+  , parts         :: [Particle]
 } deriving (Show, Read, Generic)
 
 instance FromJSON Position
