0e3deea2ddb0a429aae124f778fd8db67dd134ec
Yishuai Li <yishuai@cis.upenn.edu>
2017-04-27 13:54:02 -0400

    add some comments

--- src/Simulation.hs
+++ src/Simulation.hs
@@ -12,23 +12,27 @@ import           Types
 -- number of (simulated) seconds.
 --
 moveParticle :: Double -> Particle -> Particle
-moveParticle dt (Particle m c (Pos x y z) (Vel vx vy vz)) =
-  Particle m c (Pos (x + dt * vx) (y + dt * vy) (z + dt * vz)) (Vel vx vy vz)
+moveParticle dt p@(Particle m c (Pos x y z) (Vel vx vy vz)) =
+  p { ppos = Pos (x + dt * vx) (y + dt * vy) (z + dt * vz) }
 
+-- | Accelerate a particle in dependence on the field for the given
+-- number of (simulated) seconds.
 accelerate :: Double -> Field -> Particle -> Particle
 accelerate dt f p@(Particle mass c pos vp) = let
   a = f ./. p
   vx = velx vp + accx a * dt
   vy = vely vp + accy a * dt
   vz = velz vp + accz a * dt in
-    Particle mass c pos (Vel vx vy vz)
+    p { pvel = Vel vx vy vz }
 
+-- | Update the force of sampling points
 updateSample :: Field -> [Sample] -> [Sample]
 updateSample f samps = let
   vSamples = fromList samps
   newSamps = V.map (\s -> s { sfor = f ./ posPart (spos s)} ) vSamples in
     V.toList newSamps
 
+-- | Progressing the world state
 advanceWorld :: Double -> World -> World
 advanceWorld dt world = let
   particles    :: Vector Particle
@@ -44,13 +48,8 @@ advanceWorld dt world = let
     world { parts = R.toList newParticles,
             samples = updateSample field $ samples world }
 
--- -- Move a particle according to its velocity for the given
--- -- number of (simulated) seconds.
--- --
--- moveParticle :: Float -> Particle -> Particle
--- moveParticle dt (Particle m (Pos x y) (Vel vx vy)) =
---   Particle m (Pos (x + dt * vx) (y + dt * vy)) (Vel vx vy)
-
+-- * Original implementation
+--
 -- -- Accelerate a particle in dependence on the gravitational force
 -- -- exerted by all other particles for
 -- -- the given number of (simulated) seconds.
@@ -65,7 +64,7 @@ advanceWorld dt world = let
 --       let (Acc ax ay) = force myParticle otherParticle
 --       in
 --         Particle m pos (Vel (vx + dt * ax) (vy + dt * ay))
-
+--
 -- -- Progressing the world state
 -- --
 -- advanceWorld :: Float -> World -> World
